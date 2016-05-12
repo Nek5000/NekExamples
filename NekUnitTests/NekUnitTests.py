c149daef2e0376ea3fe8b299a0af4d325618d4a1
Ron Rahaman <rahaman@cs.uchicago.edu>
2016-05-12 14:53:49 -0500

    Added 3dbox tests

--- NekUnitTests/NekUnitTests.py
+++ NekUnitTests/NekUnitTests.py
@@ -12,7 +12,7 @@ class TurbChannelPnPn(TurbChannel):
     lz2 = 'lz1'
 
     @serial_test
-    def test_GmresSerial(self):
+    def test_gmres_serial(self):
         test_val = self.get_value(label='gmres: ', column=7,)
         self.assertAlmostEqual(test_val, 0., delta=95.)
 
@@ -29,12 +29,12 @@ class TurbChannelPnPn2(TurbChannel):
     lz2 = 'lz1-2'
 
     @serial_test
-    def test_GmresSerial(self):
+    def test_gmres_serial(self):
         test_val = self.get_value(label='gmres: ', column=6)
         self.assertAlmostEqual(test_val, 0., delta=26.)
 
     @parallel_test
-    def test_GmresParallel(self):
+    def test_gmres_parallel(self):
         test_val = self.get_value(label='gmres: ', column=6)
         self.assertAlmostEqual(test_val, 0., delta=26.)
 
@@ -48,46 +48,36 @@ class TurbChannelPnPn2(TurbChannel):
 #  3dbox, b3d
 ###############################################################################
 
-#
-# class ThreeDBoxPnPn(NekTestCase):
-#
-#     example_subdir = '3dbox'
-#     rea_file       = 'b3d'
-#
-#     serial_script     = 'nek10s'
-#     serial_log_suffix = '.pn-pn.serial'
-#
-#     parallel_script     = 'nek10steps'
-#     parallel_log_suffix = '.pn-pn.parallel'
-#
-#     lx2 = 'lx1'
-#     ly2 = 'ly1'
-#     lz2 = 'lz1'
-#
-#     def setUpClass(cls):
-#         cls.get_opts()
-#         run_meshgen('genbox', ['b3d.box'],
-#                     cwd=os.path.join(cls.examples_root, cls.example_subdir))
-#         mvn('box', 'b3d', cwd=os.path.join(cls.examples_root, cls.example_subdir))
-#         run_nek_script()
-#
-#     def test_EndOfTimeStepLoopSerial(self):
-#         cls     = self.__class__
-#         logfile = cls.parallel_logs["1"] if cls.ifmpi else cls.serial_log
-#         phrase  = get_phrase("end of time-step loop", logfile)
-#         self.assertIsNotNone(phrase)
-#
-#     @skip_unless_mpi
-#     def test_EndOfTimeStepLoopParallel(self):
-#         cls     = self.__class__
-#         logfile = cls.parallel_logs["4"]
-#         phrase  = get_phrase("end of time-step loop", logfile)
-#         self.assertIsNotNone(phrase)
-#
-# if __name__ == '__main__':
-#     unittest.main()
-#
-# class BThreeD(NekTestCase)
-#
-#     example_subdir = '3dbox'
+@pn_pn_testcase
+class ThreeDBoxPnPn(ThreeDBox):
 
+    lx = 'lx1'
+    ly = 'ly1'
+    lz = 'lz1'
+
+    @serial_test
+    def test_endOfTimeStepLoop_serial(self):
+        phrase = self.get_phrase("end of time-step loop")
+        self.assertIsNotNone(phrase)
+
+    @parallel_test
+    def test_endOfTimeStepLoop_parallel(self):
+        phrase = self.get_phrase("end of time-step loop")
+        self.assertIsNotNone(phrase)
+
+@pn_pn_2_testcase
+class ThreeDBoxPnPn2(ThreeDBox):
+
+    lx = 'lx1-2'
+    ly = 'ly1-2'
+    lz = 'lz1-2'
+
+    @serial_test
+    def test_endOfTimeStepLoop_serial(self):
+        phrase = self.get_phrase("end of time-step loop")
+        self.assertIsNotNone(phrase)
+
+    @parallel_test
+    def test_endOfTimeStepLoop_parallel(self):
+        phrase = self.get_phrase("end of time-step loop")
+        self.assertIsNotNone(phrase)
