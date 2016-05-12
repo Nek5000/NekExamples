20d3234b4803cb346827004e07ab2e636f439427
Ron Rahaman <rahaman@cs.uchicago.edu>
2016-05-12 16:39:20 -0500

    Changed method names to match labels in Analysis.py

--- NekUnitTests/NekUnitTests.py
+++ NekUnitTests/NekUnitTests.py
@@ -12,14 +12,20 @@ class TurbChannelPnPn(TurbChannel):
     lz2 = 'lz1'
 
     @serial_test
-    def test_gmres_serial(self):
+    def test_serialIter(self):
         test_val = self.get_value(label='gmres: ', column=7,)
         self.assertAlmostEqual(test_val, 0., delta=95.)
 
     @parallel_test
-    def test_gmres_parallel(self):
+    def test_parallelIter(self):
         test_val = self.get_value(label='gmres: ', column=7)
         self.assertAlmostEqual(test_val, 0., delta=95.)
+
+    @serial_test
+    def test_serialTime(self):
+        test_val = self.get_value(label='total solver time', column=2)
+        self.assertAlmostEqual(test_val, 0.1, delta=200.)
+
 
 @pn_pn_2_testcase
 class TurbChannelPnPn2(TurbChannel):
@@ -29,14 +35,19 @@ class TurbChannelPnPn2(TurbChannel):
     lz2 = 'lz1-2'
 
     @serial_test
-    def test_gmres_serial(self):
+    def test_serialIter(self):
         test_val = self.get_value(label='gmres: ', column=6)
         self.assertAlmostEqual(test_val, 0., delta=26.)
 
     @parallel_test
-    def test_gmres_parallel(self):
+    def test_parallelIter(self):
         test_val = self.get_value(label='gmres: ', column=6)
         self.assertAlmostEqual(test_val, 0., delta=26.)
+
+    @serial_test
+    def test_serialTime(self):
+        test_val = self.get_value(label='total solver time', column=2)
+        self.assertAlmostEqual(test_val, 0.1, delta=140.)
 
 ###############################################################################
 #  2d_eigtest
@@ -56,12 +67,12 @@ class ThreeDBoxPnPn(ThreeDBox):
     lz2 = 'lz1'
 
     @serial_test
-    def test_endOfTimeStepLoop_serial(self):
+    def test_serial(self):
         phrase = self.get_phrase("end of time-step loop")
         self.assertIsNotNone(phrase)
 
     @parallel_test
-    def test_endOfTimeStepLoop_parallel(self):
+    def test_parallel(self):
         phrase = self.get_phrase("end of time-step loop")
         self.assertIsNotNone(phrase)
 
@@ -73,12 +84,12 @@ class ThreeDBoxPnPn2(ThreeDBox):
     lz2 = 'lz1-2'
 
     @serial_test
-    def test_endOfTimeStepLoop_serial(self):
+    def test_serial(self):
         phrase = self.get_phrase("end of time-step loop")
         self.assertIsNotNone(phrase)
 
     @parallel_test
-    def test_endOfTimeStepLoop_parallel(self):
+    def test_parallel(self):
         phrase = self.get_phrase("end of time-step loop")
         self.assertIsNotNone(phrase)
 
@@ -94,17 +105,17 @@ class AxiPnPn(Axi):
     lz2 = 'lz1'
 
     @serial_test
-    def test_pres_serial(self):
+    def test_serialIter(self):
         val = self.get_value('PRES: ', column=4)
         self.assertAlmostEqual(val, 0., delta=76.)
 
     @parallel_test
-    def test_pres_parallel(self):
+    def test_parallelIter(self):
         val = self.get_value('PRES: ', column=4)
         self.assertAlmostEqual(val, 0., delta=76.)
 
     @serial_test
-    def test_totalSolverTime_serial(self):
+    def test_serialTime(self):
         val = self.get_value('total solver time', column=2)
         self.assertAlmostEqual(val, 0.1, delta=2)
 
@@ -117,17 +128,17 @@ class AxiPnPn2(Axi):
     lz2 = 'lz1'
 
     @serial_test
-    def test_uPress_serial(self):
+    def test_serialIter(self):
         val = self.get_value('U-Press ', column=5)
         self.assertAlmostEqual(val, 0., delta=104.)
 
     @parallel_test
-    def test_uPress_parallel(self):
+    def test_parallelIter(self):
         val = self.get_value('U-Press ', column=5)
         self.assertAlmostEqual(val, 0., delta=104.)
 
     @serial_test
-    def test_totalSolverTime_serial(self):
+    def test_serialTime(self):
         val = self.get_value('total solver time', column=2)
         self.assertAlmostEqual(val, 0.1, delta=4)
 
@@ -149,27 +160,27 @@ class BlasiusPnPn(Blasius):
     lz2 = 'lz1'
 
     @serial_test
-    def test_gmres_serial(self):
+    def test_serialIter(self):
         test_val = self.get_value('gmres: ', column=7,)
         self.assertAlmostEqual(test_val, 0., delta=162.)
 
     @parallel_test
-    def test_gmres_parallel(self):
+    def test_parallelIter(self):
         test_val = self.get_value('gmres: ', column=7,)
         self.assertAlmostEqual(test_val, 0., delta=162.)
 
     @serial_test
-    def test_delta_serial(self):
+    def test_serialError(self):
         test_val = self.get_value('delta', column=5,)
         self.assertAlmostEqual(test_val, 1.26104, delta=1e-05)
 
     @parallel_test
-    def test_delta_parallel(self):
+    def test_parallelError(self):
         test_val = self.get_value('delta', column=5,)
         self.assertAlmostEqual(test_val, 1.26104, delta=1e-05)
 
     @serial_test
-    def test_totalSolverTime_serial(self):
+    def test_serialTime(self):
         test_val = self.get_value('total solver time', 2)
         self.assertAlmostEqual(test_val, 0.1, delta=30)
 
@@ -181,26 +192,26 @@ class BlasiusPnPn2(Blasius):
     lz2 = 'lz1'
 
     @serial_test
-    def test_gmres_serial(self):
+    def test_serialIter(self):
         test_val = self.get_value('gmres: ', column=6,)
         self.assertAlmostEqual(test_val, 0., delta=125.)
 
     @parallel_test
-    def test_gmres_parallel(self):
+    def test_parallelIter(self):
         test_val = self.get_value('gmres: ', column=6,)
         self.assertAlmostEqual(test_val, 0., delta=125.)
 
     @serial_test
-    def test_delta_serial(self):
+    def test_serialError(self):
         test_val = self.get_value('delta', column=5,)
         self.assertAlmostEqual(test_val, 1.26104, delta=1e-05)
 
     @parallel_test
-    def test_delta_parallel(self):
+    def test_parallelError(self):
         test_val = self.get_value('delta', column=5,)
         self.assertAlmostEqual(test_val, 1.26104, delta=1e-05)
 
     @serial_test
-    def test_totalSolverTime_serial(self):
+    def test_serialTime(self):
         test_val = self.get_value('total solver time', 2)
         self.assertAlmostEqual(test_val, 0.1, delta=30)
