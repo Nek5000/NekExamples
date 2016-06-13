bbcf9a763511a09eb52e8462729a37ad8a02f92c
Ron Rahaman <rahaman@gmail.com>
2016-06-13 11:09:05 -0500

    Refactored fs_hydro test case

--- NekUnitTests.py
+++ NekUnitTests.py
@@ -1111,80 +1111,72 @@ class Fs2_StdWv(NekTestCase):
 # ####################################################################
 # #  fs_hydro: fs_hydro.rea
 # ####################################################################
-#
-# class FsHydro(NekTestCase):
-#     example_subdir = 'fs_hydro'
-#     rea_file       = 'fs_hydro'
-#
-#     def setUp(self):
-#         self.build_tools(['genmap'])
-#         self.run_genmap()
-#
-#     @pn_pn_serial
-#     def test_PnPn_Serial(self):
-#         self.config_size(lx='lx1', ly='ly1', lz='lz1')
-#         self.build_nek()
-#         self.run_nek(step_limit=1000)
-#
-#         phrase = self.get_phrase_from_log("ABORT: ")
-#         self.assertIsNotNullDelayed(phrase, 'ABORT: ')
-#         self.assertDelayedFailures()
-#
-#
-#
 
-#
-# @pn_pn_testcase
-# class FsHydroPnPn(FsHydro):
-#
-#     # TODO: These tests fail in legacy test suite and should be fixed
-#
-#     lx2 = 'lx1'
-#     ly2 = 'ly1'
-#     lz2 = 'lz1'
-#
-#     @serial_test
-#     def test_serial(self):
-#         phrase = self.get_phrase("ABORT: ")
-#         self.assertIsNotNone(phrase)
-#
-#     @parallel_test
-#     def test_parallel(self):
-#         phrase = self.get_phrase("ABORT: ")
-#         self.assertIsNotNone(phrase)
-#
-# @pn_pn_2_testcase
-# class FsHydroPnPn2(FsHydro):
-#
-#     lx2 = 'lx1-2'
-#     ly2 = 'ly1-2'
-#     lz2 = 'lz1'
-#
-#     @serial_test
-#     def test_serialIter(self):
-#         test_val = self.get_value('gmres: ', column=-6,)
-#         self.assertAlmostEqual(test_val, 0., delta=108.)
-#
-#     @parallel_test
-#     def test_parallelIter(self):
-#         test_val = self.get_value('gmres: ', column=-6,)
-#         self.assertAlmostEqual(test_val, 0., delta=108.)
-#
-#     @serial_test
-#     def test_serialError(self):
-#         test_val = self.get_value('AMP', column=-2, line=-1)
-#         self.assertAlmostEqual(test_val, -6.4616452E-05, delta=2e-03)
-#
-#     @parallel_test
-#     def test_parallelError(self):
-#         test_val = self.get_value('AMP', column=-2, line=-1)
-#         self.assertAlmostEqual(test_val, -6.4616452E-05, delta=2e-03)
-#
-#     @serial_test
-#     def test_serialTime(self):
-#         test_val = self.get_value('total solver time', column=-2,)
-#         self.assertAlmostEqual(test_val, 0.1, delta=200)
-#
+class FsHydro(NekTestCase):
+    example_subdir = 'fs_hydro'
+    rea_file       = 'fs_hydro'
+
+    def setUp(self):
+        self.build_tools(['genmap'])
+        self.run_genmap()
+
+    @pn_pn_serial
+    def test_PnPn_Serial(self):
+        # TODO: test fails in here and in legacy tests
+        self.config_size(lx='lx1', ly='ly1', lz='lz1')
+        self.build_nek()
+        self.run_nek(step_limit=1000)
+
+        phrase = self.get_phrase_from_log("ABORT: ")
+        self.assertIsNotNullDelayed(phrase, 'ABORT')
+        self.assertDelayedFailures()
+
+    @pn_pn_parallel
+    def test_PnPn_Parallel(self):
+        # TODO: test fails in here and in legacy tests
+        self.config_size(lx='lx1', ly='ly1', lz='lz1')
+        self.build_nek()
+        self.run_nek(step_limit=1000)
+
+        phrase = self.get_phrase_from_log("ABORT: ")
+        self.assertIsNotNullDelayed(phrase, 'ABORT')
+        self.assertDelayedFailures()
+
+    @pn_pn_2_serial
+    def test_PnPn2_Serial(self):
+        self.config_size(lx='lx1-2', ly='ly1-2', lz='lz1')
+        self.build_nek()
+        self.run_nek(step_limit=1000)
+
+        solver_time = self.get_value_from_log('total solver time', column=-2,)
+        self.assertAlmostEqualDelayed(solver_time, target_val=0.1, delta=200, label='total solver time')
+
+        gmres = self.get_value_from_log('gmres: ', column=-6,)
+        self.assertAlmostEqualDelayed(gmres, target_val=0., delta=108., label='gmres')
+
+        amp = self.get_value_from_log('AMP', column=-2, row=-1)
+        self.assertAlmostEqualDelayed(amp, target_val=-6.4616452E-05, delta=2e-03, label='AMP')
+
+        self.assertDelayedFailures()
+
+    @pn_pn_2_parallel
+    def test_PnPn2_Parallel(self):
+        self.config_size(lx='lx1-2', ly='ly1-2', lz='lz1')
+        self.build_nek()
+        self.run_nek(step_limit=1000)
+
+        solver_time = self.get_value_from_log('total solver time', column=-2,)
+        self.assertAlmostEqualDelayed(solver_time, target_val=0.1, delta=200, label='total solver time')
+
+        gmres = self.get_value_from_log('gmres: ', column=-6,)
+        self.assertAlmostEqualDelayed(gmres, target_val=0., delta=108., label='gmres')
+
+        amp = self.get_value_from_log('AMP', column=-2, row=-1)
+        self.assertAlmostEqualDelayed(amp, target_val=-6.4616452E-05, delta=2e-03, label='AMP')
+
+        self.assertDelayedFailures()
+
+
 # ####################################################################
 # #  hemi; hemi
 # ####################################################################
